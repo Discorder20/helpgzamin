172930522756e7ffb170677706c7299b67b98d51
Michał Borzuchowski <michalekborzuch@gmail.com>
2026-06-16 14:09:34 +0200

    konsolowka fix

--- konsolowa/main.py
+++ konsolowa/main.py
@@ -1,7 +1,7 @@
 import datetime
 
 class Notatka:
-    def __init__(self, tytul, tresc="", priorytetowa=False, dataUtworzenia=datetime.datetime.now()):
+    def __init__(self, tytul, tresc="", priorytetowa=False, dataUtworzenia=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")):
         self.tytul = tytul
         self.tresc = tresc
         self.priorytetowa = priorytetowa
@@ -17,9 +17,9 @@ class Notatka:
         print(self.dataUtworzenia)
         print("_" * 15)
 
-notatka = Notatka("Lekarz", "Wizyta u laryngologa", True, "12.08.2025")
+notatka = Notatka("Lekarz", "Wizyta u laryngologa", True, "12.08.2025 14:01:25")
 tytul = input("Podaj tytuł notatki:")
-notatkaUzytkownika = Notatka()
+notatkaUzytkownika = Notatka(tytul)
 notatka.wypiszNotatke()
 notatkaUzytkownika.wypiszNotatke()
         
