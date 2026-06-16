4f8151a5aa62f4225151ce554dc5880abb31c8a5
Michał Borzuchowski <michalekborzuch@gmail.com>
2026-06-16 14:46:54 +0200

    added docs

--- konsolowa/main.py
+++ konsolowa/main.py
@@ -7,6 +7,13 @@ class Notatka:
         self.priorytetowa = priorytetowa
         self.dataUtworzenia = dataUtworzenia
 
+#    **********************************************
+#    nazwa funkcji: wypiszNotatke
+#    opis funkcji: funkcja wypisuje tytuł, treść i datę notatki. W przypadku notatki priorytetowej informuje o swojej ważności
+#    parametry: parametr self - self jest parametrem automatycznie określanym przez program odpowiadającym aktualnemu obiektowi, nie należy go podawać przy wywołaniu funkcji
+#    zwracany typ i opis: brak
+#    autor: 000000000000
+#    ***********************************************
     def wypiszNotatke(self):
         print("_" * 15)
         if self.priorytetowa == True:
